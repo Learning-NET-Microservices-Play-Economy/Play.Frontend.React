bf80d72d6146867bbf7ddc3a11bee3c89bb5cfb5
Mozart Alkhateeb <mozart.alkhateeb@gmail.com>
2022-05-31 02:41:01 +0300

    fix: update API urls

--- public/config.js
+++ public/config.js
@@ -7,8 +7,8 @@
 // Warning: this file will not be transpiled by Babel and cannot contain
 // any syntax that is not yet supported by your targeted browsers.
 
-window.CATALOG_SERVICE_URL = 'https://localhost:5001'
+window.CATALOG_SERVICE_URL = 'https://localhost:7273'
 window.CATALOG_ITEMS_API_URL = `${window.CATALOG_SERVICE_URL}/items`
-window.INVENTORY_SERVICE_URL = 'https://localhost:5005'
+window.INVENTORY_SERVICE_URL = 'https://localhost:7227'
 window.INVENTORY_ITEMS_API_URL = `${window.INVENTORY_SERVICE_URL}/items`
 window.RABBITMQ_URL = 'http://localhost:15672'
